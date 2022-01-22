28dc79146bd4bfa392f5309d1c6fc4a8d0867d6a
Ceyhun-gl <ceyhungul512.com>
2022-01-22 10:31:01 +0300

    initial commit v1.1

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -3,14 +3,13 @@ import 'package:fluttergetx/models/model.dart';
 import 'package:get/get.dart';
 
 class Controller extends GetxController {
-  RxList<PersonInfo> Person = <PersonInfo>[].obs;
+  var Person = <PersonInfo>[].obs;
 
-  void Add(String text1, String text2, String text3) {
-    Person.add(PersonInfo(text1, text2, text3));
+  void Add(String name, String city, String age) {
+    Person.add(PersonInfo(name, city, age));
     print("Kayit olusturuldu!");
-    debugPrint("Name : "+text1+" City : "+text2+" Age : "+text3);
+    debugPrint("Name : "+name+" City : "+city+" Age : "+age);
 
-    print(Person.length.toString());
     update();
   }
 
